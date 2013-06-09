6ec1e8d1229db21efc25e5baea6f939a5c31acd1
nakednous <nakednous@gmail.com>
2013-06-09 22:34:39 +0000

    Renaming Abstract3DOFDevice to DOF3Device

--- src/remixlab/remixcam/device/DOF3Device.java
+++ src/remixlab/remixcam/device/DOF3Device.java
@@ -3,8 +3,8 @@ package remixlab.remixcam.device;
 import remixlab.remixcam.core.AbstractScene;
 import remixlab.remixcam.profile.DOF3Profile;
 
-public class Abstract3DOFDevice extends AbstractMotionDevice {
-	public Abstract3DOFDevice(AbstractScene scn, String n) {
+public class DOF3Device extends AbstractMotionDevice {
+	public DOF3Device(AbstractScene scn, String n) {
 		super(scn, n);
 		camProfile = new DOF3Profile();
 		frameProfile = new DOF3Profile();
